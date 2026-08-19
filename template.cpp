24f1809c3c765d93188c7fb2a5fa4edca7ea8315
Hoyneed <116987559+hoyneed@users.noreply.github.com>
2026-08-19 20:11:22 +0900

    Add template for multiple test case handling
    Initial implementation of a template for handling multiple test cases.

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+// #include <cstdio>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	//(void)freopen("input.txt", "r", stdin);
+	cin >> T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		/*
+			 이 부분에 여러분의 알고리즘 구현이 들어갑니다.
+		 */
+		/////////////////////////////////////////////////////////////////////////////////////////////
+	}
+	return 0;
+}
