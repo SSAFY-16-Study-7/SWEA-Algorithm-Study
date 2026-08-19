bf475d4fbe0f7456d16f4badcc444f28c1cbec78
Hoyneed <116987559+hoyneed@users.noreply.github.com>
2026-08-19 20:56:38 +0900

    Optimize input and clean up comments in template.cpp
    Removed comments and added input optimization.

--- template.cpp
+++ template.cpp
@@ -4,20 +4,15 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 	int test_case;
 	int T;
 	//(void)freopen("input.txt", "r", stdin);
 	cin >> T;
-	/*
-	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-	*/
 	for(test_case = 1; test_case <= T; ++test_case)
 	{
-		/////////////////////////////////////////////////////////////////////////////////////////////
-		/*
-			 이 부분에 여러분의 알고리즘 구현이 들어갑니다.
-		 */
-		/////////////////////////////////////////////////////////////////////////////////////////////
+		
 	}
 	return 0;
 }
