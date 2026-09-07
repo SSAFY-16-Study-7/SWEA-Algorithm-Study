a4c298f1c9185776d9ac27da9bdfaafda5a10d50
hoyneed <hoyneed@gmail.com>
2026-09-07 13:31:33 +0900

    feat: SWEA 1861. 정사각형 방 풀이 최적화

--- week07/hoyneed/SWEA_1861.cpp
+++ week07/hoyneed/SWEA_1861.cpp
@@ -1,7 +1,6 @@
 #include <algorithm>
 #include <array>
 #include <iostream>
-#include <queue>
 
 using namespace std;
 namespace {
@@ -10,7 +9,7 @@ struct Room {
   int num;
   Room() = default;
   Room(int y, int x, int num) : y(y), x(x), num(num) {}
-  bool operator<(const Room& other) const { return num < other.num; }
+  bool operator<(const Room& other) const { return num > other.num; }
 };
 constexpr int MAX_N = 1e3;
 constexpr int DY[4]{-1, 0, 1, 0};
@@ -19,24 +18,24 @@ constexpr int DX[4]{0, -1, 0, 1};
 int N;
 array<array<int, MAX_N>, MAX_N> rooms;
 array<array<int, MAX_N>, MAX_N> dp;
-priority_queue<Room> pq;
+array<Room, MAX_N * MAX_N + 1> arr;
 }  // namespace
 
 static void solve(const int test_case) {
   cin >> N;
-  pq = {};
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
       cin >> rooms[i][j];
       dp[i][j] = 1;
-      pq.emplace(i, j, rooms[i][j]);
+      arr[i * N + j] = Room(i, j, rooms[i][j]);
     }
   }
+  // 숫자 내림차순 정렬
+  sort(arr.begin(), arr.begin() + N * N);
   int ans_num = -1;
   int max_cnt = 0;
-  while (!pq.empty()) {
-    Room cur = pq.top();
-    pq.pop();
+  for (int i = 0; i < N * N; i++) {
+    const Room& cur = arr[i];
     // 가장 작은 숫자가 답이므로 >= 로 처리
     if (dp[cur.y][cur.x] >= max_cnt) {
       max_cnt = dp[cur.y][cur.x];
